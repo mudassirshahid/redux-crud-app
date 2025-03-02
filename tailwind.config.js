df801e9bf555e929b01c9b819ec4eb0c1bcbf64b
Mudassir <mudassirshahid84@gmail.com>
2025-03-02 20:31:10 +0500

    fix issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  mode: "jit", // Ensure JIT mode is enabled
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
