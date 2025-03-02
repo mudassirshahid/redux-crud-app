c82e3de34c438149cf7c3b7cf983d6c3a0139182
Mudassir <mudassirshahid84@gmail.com>
2025-03-02 21:02:04 +0500

    update config file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "src/index.css";`,
+      },
+    },
+  },
 });
